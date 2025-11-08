2bd6406dc761a6dfbf011f16e760d5ff4a06ac5f
Jose Correa <jcorreaf01@iescastelar.com>
2025-11-08 18:57:50 +0100

    Creo validaciones para ejercicios y rutinas

---
+++ app/Http/Requests/RutinasStoreRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class RutinasStoreRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array {
+        return [
+            'email' => 'required|email', // trainee dueño de la rutina
+            'nombre' => 'required|string|max:255',
+            'descripcion' => 'nullable|string',
+        ];
+    }
+}

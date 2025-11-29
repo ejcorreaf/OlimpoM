84e267eb1fa2c65cc2990be7f728708adf32b950
Jose Correa <jcorreaf01@iescastelar.com>
2025-11-29 20:54:12 +0100

    actualizo app.ts para añadir footer

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from "./shared/menu/menu";
+import { FooterComponent } from './shared/footer/footer';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, MenuComponent],
+  imports: [RouterOutlet, MenuComponent, FooterComponent],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('trainPlannerAngular');
+  protected readonly title = signal('TrainPlanner');
 }

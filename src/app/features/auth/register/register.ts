85d8fa204182b19ec874b90c0fb2c023d8767ef6
Jose Correa <jcorreaf01@iescastelar.com>
2025-11-29 20:58:00 +0100

    actualizo formulario de registro con DNIs y Checkbox de política de privacidad

--- src/app/features/auth/register/register.ts
+++ src/app/features/auth/register/register.ts
@@ -1,30 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, viewChild } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 import { AuthService } from '../../../core/services/auth';
+import { Router } from '@angular/router';
+import { PrivacyModalComponent } from '../../../shared/components/privacy-modal/privacy-modal';
+import { DniValidatorDirective } from '../../../core/directives/dni-validator';
 
 @Component({
   selector: 'app-register',
   templateUrl: './register.html',
   imports: [
     ReactiveFormsModule,
-    CommonModule
+    CommonModule,
+    PrivacyModalComponent,
+    DniValidatorDirective
   ]
 })
 export class RegisterComponent {
+  privacyModal = viewChild.required<PrivacyModalComponent>('privacyModal');
 
   registerForm: FormGroup;
   errorRegistro = false;
 
   constructor(
     private fb: FormBuilder,
-    private auth: AuthService
+    private auth: AuthService,
+    private router: Router
   ) {
     this.registerForm = this.fb.group({
       name: ['', Validators.required],      
+      dni: ['', Validators.required],
       email: ['', [Validators.required, Validators.email]],
       password: ['', Validators.required],
+      privacyPolicy: [false, Validators.requiredTrue]
     });
   }
 
@@ -35,15 +44,28 @@ export class RegisterComponent {
 
     this.auth.register(this.registerForm.value).subscribe({
       next: () => {
-        // TODO: redirigir o mostrar modal de éxito
+        this.router.navigate(['/login']);
       },
       error: (err) => {
         if (err.error?.errors?.email) {
           this.registerForm.get('email')?.setErrors({ emailTaken: true });
         }
-
+        if (err.error?.errors?.dni) {
+          this.registerForm.get('dni')?.setErrors({ dniTaken: true });
+        }
         this.errorRegistro = true;
       }
+    });
+  }
+
+  openPrivacyPolicy(event: Event) {
+    event.preventDefault();
+    this.privacyModal()?.show();
+  }
+
+  onPrivacyAccepted() {
+    this.registerForm.patchValue({
+      privacyPolicy: true
     });
   }
 }
